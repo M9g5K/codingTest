271424c9812596d3c1a06e1ed7a101047d0f3c74
M9g5K <k.mingyu.195@gmail.com>
2025-08-06 03:09:13 +0900

    Add files via upload

---
+++ star2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class star2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("숫자 N을 입력하세요: ");
+
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
+
+
+
+
+
+
